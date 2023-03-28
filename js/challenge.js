414899eb58860ac0fe44f7bd45d645c282b5a938
Eric Nelson <ernelson12o0@gmail.com>
2023-03-28 11:53:06 -0400

    Lab Fully Complete

--- js/challenge.js
+++ js/challenge.js
@@ -19,18 +19,24 @@ plusButton.addEventListener('click', function (event) {
 })
 
 const heartButton = document.getElementById('heart');
-const likes = document.getElementsByClassName('likes');
+const likes = document.querySelector('.likes');
 heartButton.addEventListener('click', function (event) {
+    event.preventDefault();
     event.target;
-    likes.innerText = count
+    likes.innerHTML = count;
 })
 
-
+plusButton.disabled = false;
+minusButton.disabled = false;
+heartButton.disabled = false;
 const pauseButton = document.getElementById('pause');
 pauseButton.addEventListener('click', function (event) {
   if (pauseButton.innerText === 'pause') {
     clearInterval(intervalID);
     pauseButton.innerHTML = 'resume';
+    plusButton.disabled = true;
+    minusButton.disabled = true;
+    heartButton.disabled = true;
   } else {
     count = parseInt(document.getElementById('counter').innerHTML);
     intervalID = setInterval(function() {
@@ -38,5 +44,22 @@ pauseButton.addEventListener('click', function (event) {
       document.getElementById('counter').innerHTML = count; 
     }, 1000);
     pauseButton.innerHTML = 'pause';
+    plusButton.disabled = false;
+    minusButton.disabled = false;
+    heartButton.disabled = false;
   } 
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault()
+    handleComment(event.target.comment_input.value)
+  }); 
+});
+
+function handleComment(comment) {
+  let p = document.createElement('p');
+  p.textContent = comment;
+  document.querySelector('#list').appendChild(p);
+};
